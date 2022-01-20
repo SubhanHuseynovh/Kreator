337904fc43284e26c6f7c8af7d42718301568ea8
SubhanHuseynovh <98095748+SubhanHuseynovh@users.noreply.github.com>
2022-01-20 18:43:24 +0400

    Create android.sh

---
+++ http:/android.sh
@@ -0,0 +1,26 @@
+MESAJ="AsenaUserBot Otomatik Deploy Kuruluma Hoş Geldiniz"
+MESAJ+="\nTelegram: @AsenaUserBot"
+pkg update -y
+clear
+echo -e $MESAJ
+echo "Python Yükleniyor"
+pkg install python -y
+clear
+echo -e $MESAJ
+echo "Git Yükleniyor"
+pkg install git -y
+clear
+echo -e $MESAJ
+echo "TeleThon Yükleniyor"
+pip install telethon
+echo "Repo klonlanıyor..."
+git clone https://github.com/AsenaDev/Installer
+clear
+echo -e $MESAJ
+cd Installer
+clear
+echo "Gereksinimler Yükleniyor"
+echo -e $MESAJ
+pip install wheel
+pip install -r requirements.txt
+python -m asena_installer
